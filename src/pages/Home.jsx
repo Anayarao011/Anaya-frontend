e28229ac7583635f6780932da9d93ce923a36266
Anayarao011 <anayarao011@gmail.com>
2025-02-23 11:14:06 +0530

    Project added

---
+++ src/pages/Home.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+     <img src="/ban.jpg" class="img-fluid w-100 pt-1" alt="..."/ >
+     <div class="row row-cols-2 row-cols-md-2 g-4 m-2">
+  <div class="col">
+    <div class="card">
+      <img src="/ban.jpg" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="Veg Recepies">Veg Recipes</h5>
+        <p class="card-text"> Veg Biryani – there are many varieties of biryani made in India. An authentic veg biryani is …
+Rajma chawal – It is one of the regulars at any Punjabi house for lunch or dinner. Mostly it’s …
+Bisi bele bath – It is a delicious and healthy complete meal of rice, lentils and vegetables. Bisi …
+Chana masala – there are many ways chickpeas curries are made in regional Indian cuisine..</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="/ban.jpg" class="card-img-top" alt="..."/ >
+      <div class="card-body">
+        <h5 class="Non-Veg Recepies">Non-Veg Recepies</h5>
+        <p class="card-text">Best Egg Curry Recipe. A nice change of pace and convenient on those days when I might …
+Pomfret Fish Recipe. A very easy to make masala fish marinated in a range of spices and …
+Egg Bhurji Masala. Easy Egg Bhurji Recipe with cheese is nothing but scrambled eggs with …
+Mini Chicken Quiche Recipe. The recipe we’re making today is that of a classic chicken …</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="/ban.jpg" class="card-img-top" alt="..."/ >
+      <div class="card-body">
+        <h5 class="Potato Recepies">Potato Recepies</h5>
+        <p class="card-text">Potato plants are herbaceous perennials that grow up to 1 metre (3.3 ft) high. The stems are hairy. The leaves have roughly four pairs of leaflets. The flowers range from white or pink to blue or purple; they are yellow at the centre, and are insect-pollinated.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="/ban.jpg" class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="Green Vegetables">Green Vegetables</h5>
+        <p class="card-text"> Caprese Salad with Basil & Tarragon Chimichurri. This is a caprese salad like you've never …
+Green Goddess Farro Bowl. Farro and vegetables with a thick green goddess dressing pack …
+Caramelized Broccolini & White Beans. Broccolini takes on a smoky char from the cast-iron …
+Kale & Shaved Brussels Sprouts Salad with Avocado Caesar Dressing. This salad combines …</p>
+      </div>
+    </div>
+  </div>
+</div>
+    </div>
+  )
+}
+
+export default Home
